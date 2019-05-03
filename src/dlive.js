5800e0ae18807ee728b8f8d242eb7ba34b0e1e61
timedotcc <34674720+timedotcc@users.noreply.github.com>
2019-05-04 00:41:59 +0200

    reformatting

--- src/dlive.js
+++ src/dlive.js
@@ -1,43 +1,43 @@
-'use strict'
-const EventEmitter = require('events')
-const https = require('https')
-const Websocket = require('websocket')
+"use strict";
+const EventEmitter = require("events");
+const https = require("https");
+const Websocket = require("websocket");
 
 class dlive extends EventEmitter {
-  constructor () {
-    super()
-    this.authkey = ''
-    this.channel = ''
-    this.https = https
-    this.WebsocketClient = Websocket.client
-    this.client = new this.WebsocketClient()
-  }
+    constructor () {
+        super();
+        this.authkey = "";
+        this.channel = "";
+        this.https = https;
+        this.WebsocketClient = Websocket.client;
+        this.client = new this.WebsocketClient();
+    }
 
-  get getAuthkey () {
-    return this.authkey
-  }
+    get getAuthkey() {
+        return this.authkey;
+    }
 
-  get getChannel () {
-    return this.channel
-  }
+    get getChannel() {
+        return this.channel;
+    }
 
-  get getBlockChainUsername () {
-    return this.blockChainUsername
-  }
+    get getBlockChainUsername() {
+        return this.blockChainUsername;
+    }
 
-  set setBlockChainUsername (username) {
-    this.blockChainUsername = username
-  }
+    set setBlockChainUsername(username) {
+        this.blockChainUsername = username;
+    }
 
-  set setAuthkey (authkey) {
-    this.authkey = authkey
-  }
+    set setAuthkey(authkey) {
+        this.authkey = authkey;
+    }
 
-  set setChannel (channel) {
-    this.channel = channel
-  }
+    set setChannel(channel) {
+        this.channel = channel;
+    }
 }
 
 module.exports = {
-  dlive: dlive
-}
+    dlive: dlive
+};
